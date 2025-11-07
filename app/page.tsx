12a7e2c23e94c5765df6785a20f159c8db4169dd
Abdo293 <abdoelsayed293@gmail.com>
2025-11-07 13:53:21 +0200

    end task

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
 "use client";
-import AddProductForm from "@/components/add-products/AddProductsForm";
 import AnalyticsPage from "@/components/home/AnalysisPage";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { fetchProducts } from "@/store/slices/productsSlice";
 import { useEffect } from "react";
 export default function Home() {
   const dispatch = useAppDispatch();
-  const { products, loading, error } = useAppSelector(
-    (state) => state.products
-  );
+  const { products } = useAppSelector((state) => state.products);
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
-
-  console.log(products);
 
   return (
     <div>
